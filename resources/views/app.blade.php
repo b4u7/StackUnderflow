87c250957ebdfcfdff1bc00b8082f9be61d61bdc
b4u7 <10912932+b4u7@users.noreply.github.com>
2021-07-10 16:24:02 +0100

    fix: asset() -> mix()

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,12 +17,12 @@
     </link>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
 
     @routes
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('/js/app.js') }}" defer></script>
 </head>
 <body>
 @inertia
