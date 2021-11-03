e2aee2bccebc38c4d671430464444e23a6c02242
b4u7 <10912932+b4u7@users.noreply.github.com>
2021-11-03 18:29:46 +0000

    feat: user profile page

---
+++ resources/assets/js/Pages/User/Show.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="section">
+    <!-- w-full max-w-8xl mx-auto -->
+    <div class="container">
+      <div class="user-profile">
+        <div class="user-profile__header">
+          <div class="user-profile__header__background"></div>
+          <div class="user-profile__header__name"></div>
+          <div class="user-profile__header__username"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  props: {
+    User: {
+      type: Object, // ?
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped></style>
