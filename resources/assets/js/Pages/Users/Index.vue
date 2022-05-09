764d8048b27413388b11a8ae159130b2743d27a6
Daniel <10912932+b4u7@users.noreply.github.com>
2022-05-09 12:16:29 +0100

    fix: remove `this`

--- resources/assets/js/Pages/Users/Index.vue
+++ resources/assets/js/Pages/Users/Index.vue
@@ -15,12 +15,12 @@
       <div class="grid grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
         <user-card v-for="user of users.data" :key="user.id" :user="user" />
       </div>
-      <div v-if="this.users.prev_page_url || this.users.next_page_url" class="buttons flex justify-between">
-        <button v-if="this.users.prev_page_url" class="button button--primary" type="button" @click.prevent="prevPage">
+      <div v-if="users.prev_page_url || users.next_page_url" class="buttons flex justify-between">
+        <button v-if="users.prev_page_url" class="button button--primary" type="button" @click.prevent="prevPage">
           Previous
         </button>
         <button
-          v-if="this.users.next_page_url"
+          v-if="users.next_page_url"
           class="ml-auto button button--primary"
           type="button"
           @click.prevent="nextPage"
