ede58e1e567d40ae5a3811d75c3d2c3015956249
Daniel <10912932+b4u7@users.noreply.github.com>
2022-05-10 19:59:25 +0100

    ref: added return type

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,15 +5,14 @@ namespace App\Http\Controllers;
 use Auth;
 use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class HomeController extends Controller
 {
     /**
      * Show the application dashboard.
-     *
-     * @return RedirectResponse | \Inertia\Response
      */
-    public function index()
+    public function index(): RedirectResponse|Response
     {
         if (Auth::check()) {
             return redirect()->route('questions.index');
