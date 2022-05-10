ec1f343ae4ca5ccf664906f203228e10539da469
Daniel <10912932+b4u7@users.noreply.github.com>
2022-05-10 19:59:04 +0100

    fix: increased spacing between search and users list

--- resources/assets/js/Pages/Users/Index.vue
+++ resources/assets/js/Pages/Users/Index.vue
@@ -1,21 +1,18 @@
 <template>
   <section class="section mt-8">
     <div class="container">
-      <form class="form mb-4" @submit.prevent="search">
-        <div class="form__group">
-          <input
-            type="text"
-            name="search"
-            class="form__group__control"
-            v-model="form.user"
-            placeholder="Search for users"
-          />
-        </div>
+      <form class="mb-8" @submit.prevent="search">
+        <input
+          class="form__group__control"
+          type="text"
+          placeholder="Search for users"
+          v-model="form.user"
+        />
       </form>
       <div class="grid grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
         <user-card v-for="user of users.data" :key="user.id" :user="user" />
       </div>
-      <div v-if="users.prev_page_url || users.next_page_url" class="buttons flex justify-between">
+      <div v-if="users.prev_page_url || users.next_page_url" class="flex justify-between">
         <button v-if="users.prev_page_url" class="button button--primary" type="button" @click.prevent="prevPage">
           Previous
         </button>
