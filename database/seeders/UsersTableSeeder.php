e2dd694164bbabbbf157e0eafc93d76c78cac606
Daniel <10912932+b4u7@users.noreply.github.com>
2022-05-11 20:07:48 +0100

    feat: generate 500 instead of 50 users

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -12,6 +12,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->count(50)->create();
+        User::factory()->count(500)->create();
     }
 }
