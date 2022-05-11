dbf004fda6a6031b659520101edc62ad214ec2ae
Daniel <10912932+b4u7@users.noreply.github.com>
2022-05-11 20:01:18 +0100

    feat: `hero--full` -> `hero--medium`

--- resources/assets/js/Pages/Index.vue
+++ resources/assets/js/Pages/Index.vue
@@ -1,15 +1,17 @@
 <template>
-  <hero class="hero--full">
+  <hero class="hero--medium">
     <template #body>
       <h1 class="hero__leading">Learn, Share, Build</h1>
       <div class="hero__cta">
         <p>
-          <Link methods="post" as="button" :href="route('register')" class="button button--warning button--xl">
+          <Link class="button button--warning button--xl" as="button" :href="route('register')">
             Join the community
             <font-awesome-icon icon="fa-solid fa-right" />
           </Link>
+        </p>
+        <p>
           or
-          <Link class="underline font-medium text-amber-400" :href="route('questions.index')">search for content</Link>
+          <Link class="underline font-medium text-yellow-400" :href="route('questions.index')">search for content</Link>
         </p>
       </div>
     </template>
@@ -22,6 +24,6 @@ import { Link } from '@inertiajs/inertia-vue'
 
 export default {
   name: 'Index',
-  components: { Hero, Link }
+  components: { Hero, Link },
 }
 </script>
