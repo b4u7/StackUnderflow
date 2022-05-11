f34aa2aba06e508346d0d6b464f839f090f922e1
Daniel <10912932+b4u7@users.noreply.github.com>
2022-05-11 20:02:10 +0100

    feat: stats-children: true on webpackConfig

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@ const path = require('path')
 
 module.exports = {
   output: {
-    chunkFilename: 'js/[name].js?id=[chunkhash]',
+    chunkFilename: 'js/[name].js?id=[chunkhash]'
   },
   resolve: {
     alias: {
-      '@': path.resolve('resources/assets/js'),
-    },
+      '@': path.resolve('resources/assets/js')
+    }
   },
+  stats: {
+    children: true
+  }
 }
