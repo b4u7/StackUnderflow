67e21d4604ffafde57af7e8d4fb99d0f14b62450
Daniel <10912932+b4u7@users.noreply.github.com>
2022-05-24 19:08:24 +0100

    feat: delete -> text-red and restore -> text-green

--- resources/assets/js/Components/Questions/Actions/FooterActions.vue
+++ resources/assets/js/Components/Questions/Actions/FooterActions.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="buttons space-x-2">
+  <div class="w-full sm:w-auto buttons space-x-4">
     <tippy :message="shareMessage" :hideOnClick="false" :open.sync="showShareTippy" trigger="manual" placement="top">
       <Link
         :class="classes"
+        class="hover:text-primary-600 focus:text-primary-800 active:text-primary-800"
         as="button"
         @mouseenter="showShareTippy = true"
         @mouseleave="showShareTippy = false"
@@ -12,15 +13,35 @@
         Share
       </Link>
     </tippy>
-    <Link v-if="canEdit" :class="classes" :href="editRoute" as="button">
+    <Link
+      v-if="canEdit"
+      :class="classes"
+      :href="editRoute"
+      class="hover:text-primary-600 focus:text-primary-800 active:text-primary-800"
+      as="button"
+    >
       <font-awesome-icon icon="fa-solid fa-pen-to-square" />
       Edit
     </Link>
-    <Link v-if="canDelete" :class="classes" :href="deleteRoute" method="delete" as="button">
+    <Link
+      v-if="canDelete"
+      :class="classes"
+      :href="deleteRoute"
+      class="hover:text-red-600 focus:text-red-800 active:text-red-800"
+      method="delete"
+      as="button"
+    >
       <font-awesome-icon icon="fa-solid fa-trash-can" />
       Delete
     </Link>
-    <Link v-if="canRestore" :href="restoreRoute" :class="classes" method="post" as="button">
+    <Link
+      v-if="canRestore"
+      :class="classes"
+      :href="restoreRoute"
+      class="hover:text-green-600 focus:text-green-800 active:text-green-800"
+      method="post"
+      as="button"
+    >
       <font-awesome-icon icon="fa-solid fa-trash-can-arrow-up" />
       Restore
     </Link>
@@ -40,51 +61,48 @@ export default {
   props: {
     canEdit: {
       type: Boolean,
-      required: true,
+      required: true
     },
     canDelete: {
       type: Boolean,
-      required: true,
+      required: true
     },
     canRestore: {
       type: Boolean,
-      required: true,
+      required: true
     },
     data: {
-      type: Object,
+      type: Object
     },
     isTrashed: {
       type: Boolean,
-      required: true,
+      required: true
     },
     shareUrl: {
       type: String,
-      required: true,
+      required: true
     },
     editRoute: {
       type: String,
-      required: true,
+      required: true
     },
     deleteRoute: {
       type: String,
-      required: true,
+      required: true
     },
     restoreRoute: {
       type: String,
-      required: true,
-    },
+      required: true
+    }
   },
   data() {
     return {
       classes: [
         'transition-all',
-        'font-semibold',
-        'hover:text-primary-600',
-        'focus:text-primary-800',
-        'active:text-primary-800',
+        'font-semibold'
       ],
       shareMessage: SHARE_MESSAGE,
-      showShareTippy: false,
+      showShareTippy: false
     }
   },
   methods: {
@@ -98,7 +116,7 @@ export default {
 
         setTimeout(() => (this.shareMessage = SHARE_MESSAGE), 1000)
       }, 1000)
-    },
-  },
+    }
+  }
 }
 </script>
