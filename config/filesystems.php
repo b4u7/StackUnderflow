6dbb2e84ede6df3f22c3ada055e71a54c166228c
Daniel <10912932+b4u7@users.noreply.github.com>
2022-07-11 18:41:20 +0100

    feat: add DO_SPACES_URL env key

--- config/filesystems.php
+++ config/filesystems.php
@@ -63,6 +63,7 @@ return [
             'endpoint' => env('DO_SPACES_ENDPOINT'),
             'region' => env('DO_SPACES_REGION'),
             'bucket' => env('DO_SPACES_BUCKET'),
+            'url' => env('DO_SPACES_URL'),
         ]
     ],
 ];
