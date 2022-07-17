96830345adbe024c145f079dd4e79d5a82f1b8db
Daniel <10912932+b4u7@users.noreply.github.com>
2022-07-17 17:36:47 +0100

    fix: remove authorize method

--- app/Http/Requests/AnswerUpdateRequest.php
+++ app/Http/Requests/AnswerUpdateRequest.php
@@ -7,24 +7,14 @@ use Illuminate\Foundation\Http\FormRequest;
 class AnswerUpdateRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, mixed>
      */
-    public function rules()
+    public function rules(): array
     {
         return [
-            //
+            'body' => 'required|string|max:30000'
         ];
     }
 }
